115bab66c9611505dfead0d124bea13ee1a805cf
FarazAhmad2 <farazahmad574@gmail.com>
2024-07-23 22:20:30 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,36 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState, useEffect, useMemo } from "react";
 
 const App = () => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+    .then(response=>response.json())
+    .then(data=>{
+      setData(data)
+      setLoading(false)
+    })
+  }, []);
+
+  const memoizedData = useMemo(() => data, [data]);
+
+  if (loading) {
+    return <h3>Loading...</h3>;
+  }
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <ul>
+        {memoizedData && memoizedData.map((item) => (
+          <li key={item.id}>
+            <h3>{item.title}</h3>
+            <p>{item.body}</p>
+          </li>
+        ))}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
